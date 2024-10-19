25d13aac091ce9256467a669275cdc5f934ebb25
jaden <asadsaifi90@gmail.com>
2024-10-19 12:16:49 -0500

    modified:   app.js 	modified:   routes/user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,14 @@ const { getUserData } = require("../services/user.service");
 var router = express.Router();
 
 router.get("/get-user", async (req, res) => {
-  let username = req?.username;
+  let username = req.query.username; // Extract username from query parameters
+  if (!username) {
+    return res.status(400).json({
+      status: false,
+      message: "Username is required",
+    });
+  }
+
   let response = await getUserData(username);
   res.json(response);
 });
